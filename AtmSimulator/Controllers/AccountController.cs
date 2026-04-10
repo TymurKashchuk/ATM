a696e3560e0f97f71a377b1558b73109bbcf12d9
TymurKashchuk <t1mu4337@gmail.com>
2026-04-10 14:09:39 +0300

    feat: add AccountController, account view model and main menu

--- AtmSimulator/Controllers/AccountController.cs
+++ AtmSimulator/Controllers/AccountController.cs
@@ -1,6 +1,38 @@
-﻿namespace AtmSimulator.Controllers
+﻿using AtmSimulator.Data;
+using AtmSimulator.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmSimulator.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() {
+            var accountId = HttpContext.Session.GetInt32("AccountId");
+            if (accountId == null) return RedirectToAction("InsertCard", "Auth");
+
+            var account = await _context.Accounts
+            .Include(a => a.Card)
+            .FirstOrDefaultAsync(a => a.Id == accountId);
+
+            if (account == null) return RedirectToAction("InsertCard", "Auth");
+
+            var viewModel = new AccountViewModel
+            {
+                OwnerName = account.OwnerName,
+                Balance = account.Balance,
+                CardNumber = account.Card!.CardNumber,
+                IsBlocked = account.Card.IsBlocked
+            };
+
+            return View(viewModel);
+        }
     }
 }
