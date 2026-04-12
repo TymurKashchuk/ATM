15b59bfc27d93d79dab54cf52444f9b98a8582a8
TymurKashchuk <t1mu4337@gmail.com>
2026-04-12 16:56:20 +0300

    add deposit actions to TransactionController

--- AtmSimulator/ViewModels/DepositViewModel.cs
+++ AtmSimulator/ViewModels/DepositViewModel.cs
@@ -7,7 +7,6 @@ namespace AtmSimulator.ViewModels
         [Required(ErrorMessage = "Введіть суму")]
         [Range(20, 100000, ErrorMessage = "Сума має бути від 20 до 100 000")]
         public decimal Amount { get; set; }
-
         public decimal CurrentBalance { get; set; }
     }
 }
