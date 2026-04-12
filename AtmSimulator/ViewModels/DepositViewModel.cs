7b230bfbde99800104ee24127a882fab86070850
TymurKashchuk <t1mu4337@gmail.com>
2026-04-12 16:42:44 +0300

    create DepositViewModel with validation

---
+++ AtmSimulator/ViewModels/DepositViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtmSimulator.ViewModels
+{
+    public class DepositViewModel
+    {
+        [Required(ErrorMessage = "Введіть суму")]
+        [Range(20, 100000, ErrorMessage = "Сума має бути від 20 до 100 000")]
+        public decimal Amount { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+    }
+}
