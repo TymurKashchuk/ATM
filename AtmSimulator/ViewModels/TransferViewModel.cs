4480217a67d9e71e8f93ef4772ea8e55d3ccbc45
TymurKashchuk <t1mu4337@gmail.com>
2026-04-13 16:48:44 +0300

    create TransferViewModel with validation rules

---
+++ AtmSimulator/ViewModels/TransferViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtmSimulator.ViewModels
+{
+    public class TransferViewModel
+    {
+        [Required(ErrorMessage = "Введіть номер картки одержувача")]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "Номер картки має містити 16 цифр")]
+        public string RecipientCardNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть суму")]
+        [Range(1, 50000, ErrorMessage = "Сума має бути від 1 до 50 000")]
+        public decimal Amount { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+        public string? RecipientName { get; set; }
+    }
+}
