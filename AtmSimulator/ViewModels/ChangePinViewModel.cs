467b015b05bc137a2175aa783fd1b133dd13a3f7
TymurKashchuk <t1mu4337@gmail.com>
2026-04-14 21:44:50 +0300

    create ChangePinViewModel with validation rules

---
+++ AtmSimulator/ViewModels/ChangePinViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtmSimulator.ViewModels
+{
+    public class ChangePinViewModel
+    {
+        [Required(ErrorMessage = "Введіть поточний PIN")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "PIN має бути 4 цифри")]
+        public string CurrentPin { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть новий PIN")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "PIN має бути 4 цифри")]
+        public string NewPin { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Підтвердіть новий PIN")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "PIN має бути 4 цифри")]
+        public string ConfirmPin { get; set; } = string.Empty;
+    }
+}
