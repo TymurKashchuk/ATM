8dd45dbe7cdaf4d5208b1281c4e1cd685fa233e0
TymurKashchuk <t1mu4337@gmail.com>
2026-04-14 21:43:52 +0300

    add PinService for PIN change logic

---
+++ AtmSimulator/Services/PinService.cs
@@ -0,0 +1,37 @@
+﻿using AtmSimulator.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmSimulator.Services
+{
+    public class PinService
+    {
+        private readonly AppDbContext _context;
+
+        public PinService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ChangePinAsync(int accountId, string currentPin, string newPin, string confirmPin)
+        {
+            if (newPin != confirmPin)
+                throw new InvalidOperationException("Новий PIN та підтвердження не збігаються");
+
+            if (newPin.Length != 4 || !newPin.All(char.IsDigit))
+                throw new InvalidOperationException("PIN має містити рівно 4 цифри");
+
+            if (currentPin == newPin)
+                throw new InvalidOperationException("Новий PIN має відрізнятись від поточного");
+
+            var card = await _context.Cards
+                .FirstOrDefaultAsync(c => c.AccountId == accountId)
+                ?? throw new InvalidOperationException("Картку не знайдено");
+
+            if (card.PinHash != currentPin)
+                throw new InvalidOperationException("Поточний PIN невірний");
+
+            card.PinHash = newPin;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
